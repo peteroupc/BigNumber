c388ca699b3da7881aa9c7f0e4568287f35e4839
Peter O <poccil14@gmail.com>
2015-02-19 10:15:00 -0500

    update

--- BigInteger.js
+++ BigInteger.js
@@ -1,6 +1,6 @@
 (function(){
 /*
-Written in 2013-2014 by Peter O.
+Written in 2013-2015 by Peter O.
 
 Parts of the code were adapted by Peter O. from
 the public-domain code from the library
@@ -9,7 +9,8 @@ CryptoPP by Wei Dai.
 Any copyright is dedicated to the Public Domain.
 http://creativecommons.org/publicdomain/zero/1.0/
 If you like this, you should donate to Peter O.
-at: http://upokecenter.com/d/
+at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
+
  */
 if(typeof StringBuilder=="undefined"){
 var StringBuilder=function(){
