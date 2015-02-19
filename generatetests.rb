e6ff8e28da31558c916e1a49be5d3de7ca22e36b
Peter O <poccil14@gmail.com>
2015-02-19 05:11:18 -0500

    add HTML demo for big number library

--- generatetests.rb
+++ generatetests.rb
@@ -3,7 +3,7 @@
 # http://creativecommons.org/publicdomain/zero/1.0/
 # 
 # If you like this, you should donate to Peter O.
-# at: http://upokecenter.com/d/
+# at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
 
 # Generates test cases for the JavaScript version.
 
